3d73947cdc3d82bffafe8eae08295688bec2d9d0
Nathan Simpson <ndsimpson01@gmail.com>
2019-05-19 11:50:18 -0600

    Filter functional for adding to list

--- src/Components/List/ItemWizard.jsx
+++ src/Components/List/ItemWizard.jsx
@@ -8,21 +8,52 @@ class ItemWizard extends Component{
     constructor(){
         super()
         this.state = {
-
+            filter: false,
+            filterValue: ''
         }
+    }
+
+    toggleFilter = () => {
+        this.setState({
+            filter: !this.state.filter
+        })
+    }
+
+    // track user inputs via local state
+    handleInput = event => {
+        let {name, value} = event.target
+        this.setState({
+            [name]: value
+        })
     }
     
     render(){
-
-        let items = this.props.items.map(item => {
-            return (
-                <ItemWizardIcons key={item.item_id} item={item} > </ItemWizardIcons>
-            )
-        })
+        let items
+        // filter our items
+        if (this.state.filter){
+            let filteredItems = this.props.items.filter( item =>  {
+                // account for case-sensitivity
+                let filter_lowercase = this.state.filterValue.toLowerCase()
+                let name_lowercase = item.name.toLowerCase()
+                let tags_lowercase = item.tags.toLowerCase()
+                return name_lowercase.includes(filter_lowercase) || tags_lowercase.includes(filter_lowercase)
+                })
+            // map the list into components
+            items = filteredItems.map(item => <ItemWizardIcons key={item.item_id} item={item} > </ItemWizardIcons>)
+        } else {
+            items = this.props.items.map(item => <ItemWizardIcons key={item.item_id} item={item} > </ItemWizardIcons>)
+        }
 
         return(
             <div>
                 ItemWizard
+                <button onClick={this.toggleFilter}> Toggle Filter </button>
+                <input 
+                        onChange={this.handleInput} 
+                        type="text" 
+                        name='filterValue' 
+                        placeholder='filter value'
+                    />
                 {items}
             </div>
         )
